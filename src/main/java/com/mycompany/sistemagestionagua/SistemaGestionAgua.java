e5e5f28396d3ae58ffd8825a036ef4b9e6ab7084
renegameportillo <renegameportillo@gmail.com>
2025-10-16 14:34:57 -0600

    Hice el formulario de Registro de usuarios

--- src/main/java/com/mycompany/sistemagestionagua/SistemaGestionAgua.java
+++ src/main/java/com/mycompany/sistemagestionagua/SistemaGestionAgua.java
@@ -51,8 +51,7 @@ public class SistemaGestionAgua {
             ex.printStackTrace();
         }
 
-<<<<<<< HEAD
-=======
+
         VistaPrincipal vista = new VistaPrincipal();
         ModeloPrincipal modelo = new ModeloPrincipal();
 
@@ -63,6 +62,6 @@ public class SistemaGestionAgua {
         System.out.println("rene soy irvin");
         System.out.println("Irvin ");
 
->>>>>>> 6475171d61caf19e6dd98e2dd0409c63dbb3f7e1
+
     }
 }
