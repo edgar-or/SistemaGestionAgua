599e50133dc9d72a84a2f0fc0028bc8bf3a742a2
edgar <ayalaalvarezedgarorlando@gmail.com>
2025-11-14 14:51:19 -0600

    cerrarSesion

--- src/main/java/com/mycompany/sistemagestionagua/modelo/Base.java
+++ src/main/java/com/mycompany/sistemagestionagua/modelo/Base.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.sistemagestionagua.modelo;
 
 import java.math.BigDecimal;
@@ -9,15 +8,17 @@ public class Base {
 
     ArrayList<Usuario> usuarios;
     ArrayList<Servicio> servicios;
-    ArrayList<ModeloRuta> rutas; 
+    ArrayList<ModeloRuta> rutas;
     ArrayList<ModeloConsumo> consumos;
+    private boolean datosInicialesCargados = false;
+
 
     public Base() {
         usuarios = new ArrayList<>();
         servicios = new ArrayList<>();
         rutas = new ArrayList<>();
-        consumos= new ArrayList<>();
-        
+        consumos = new ArrayList<>();
+
     }
 
     public boolean agregar(Usuario e) {
@@ -53,12 +54,24 @@ public class Base {
     public ArrayList<ModeloConsumo> getConsumos() {
         return consumos;
     }
-    
-    
-    
-   
-    
-    
+
+    public void cargarDatosIniciales() {
+        if (datosInicialesCargados) {
+            return; // Ya se cargaron, no repetir.
+        }
+        // === Aquí metes tus datos quemados ===
+        agregar(new Usuario("Edgar", "Ayala", "12345678-1"));
+        agregar(new Usuario("Orland0", "Alvarez", "12345678-2"));
+
+        agregarRuta(new ModeloRuta("1", "San Vicente", "San Vicente", "La Arenera", "Calle principal a Lempa"));
+        agregarRuta(new ModeloRuta("2", "San Vicente", "San Vicente", "Los Jobos", "Calle principal a Iglesia catolica"));
+
+        agregarServicio(new Servicio("12345678-1", "2025-0001", "1", 3, "12"));
+        agregarServicio(new Servicio("12345678-2", "2025-0003", "2", 4, "13"));
+
+        // marcar como cargado
+        datosInicialesCargados = true;
+    }
 
     public ArrayList<Usuario> buscarUsuario(String identificador, String busca) {
         ArrayList<Usuario> temp = new ArrayList<>();
@@ -82,54 +95,48 @@ public class Base {
 
         return temp;
     }
-    
-    
+
     public ArrayList<ModeloConsumo> buscarConsumo(String identificador, String busca) {
         ArrayList<ModeloConsumo> temp = new ArrayList<>();
         ModeloConsumo encontrado = null;
 
         for (ModeloConsumo consumo : consumos) {
             if (identificador.equalsIgnoreCase("dui")) {
-                String numCuenta = encontrarServicio(busca).getNumeroCuenta(); 
-                
+                String numCuenta = encontrarServicio(busca).getNumeroCuenta();
+
                 if (consumo.getNumeroCuenta().equals(numCuenta)) {
                     encontrado = consumo;
-                temp.add(encontrado);
+                    temp.add(encontrado);
                 }
-                
 
-            } else if (identificador.equalsIgnoreCase("cuenta")){
-                
+            } else if (identificador.equalsIgnoreCase("cuenta")) {
+
                 if (consumo.getNumeroCuenta().trim().equals(busca)) {
                     encontrado = consumo;
-                temp.add(encontrado);
+                    temp.add(encontrado);
                 }
-                
 
-            } else if (identificador.equalsIgnoreCase("nombre") ) {
-                
+            } else if (identificador.equalsIgnoreCase("nombre")) {
+
                 String dui = buscarPorNombre(busca);
-                
+
                 String serv = encontrarServicio(dui).getNumeroCuenta();
-                
+
                 if (serv.equals(consumo.getNumeroCuenta())) {
-                      encontrado = consumo;
-                temp.add(encontrado);
+                    encontrado = consumo;
+                    temp.add(encontrado);
                 }
-              
+
             }
 
         }
 
         return temp;
     }
-    
-
-
 
     public ArrayList<String> bNumCuenta(String dui) {
         ArrayList<String> temp = new ArrayList<>();
-        
+
         for (Servicio servicio : servicios) {
             if (dui.equals(servicio.getDuiPropietario())) {
                 temp.add(servicio.getnumeroCuenta());
@@ -137,8 +144,6 @@ public class Base {
         }
         return temp;
     }
-
-    
 
     public String buscarServicioPorNumCuenta(String buscar) {
         for (Servicio servicio : servicios) {
@@ -149,13 +154,14 @@ public class Base {
         }
         return null;
     }
-    public Servicio datosServicios(String numCuenta){
+
+    public Servicio datosServicios(String numCuenta) {
         for (Servicio servi : servicios) {
             if (numCuenta.equals(servi.getNumeroCuenta())) {
-                return servi; 
+                return servi;
             }
         }
-        return null; 
+        return null;
     }
 
     public String buscarServicio(String buscar) {
@@ -186,7 +192,7 @@ public class Base {
         return encontrado;
 
     }
-    
+
     public Servicio encontrarServicioPorNumCuenta(String numCuenta) {
         Servicio encontrado = null;
 
@@ -214,19 +220,18 @@ public class Base {
         return false;
 
     }
-    
+
     //arralist de metodo servicios
-    
-    
-    public String nombrePorDui(String dui){
+    public String nombrePorDui(String dui) {
         for (Usuario usuario : usuarios) {
             if (dui.equals(usuario.getDui())) {
                 return usuario.getNombre();
             }
         }
-         return null; 
+        return null;
 
     }
+
     public String buscarPorNombre(String nombre) {
         for (Usuario usuario : usuarios) {
             if (usuario.getNombre().toLowerCase().contains(nombre.toLowerCase())) {
@@ -235,8 +240,8 @@ public class Base {
         }
         return null; // si no encuentra, retorna null
     }
-    
-    public String buscarPorApellido(String apellido){
+
+    public String buscarPorApellido(String apellido) {
         for (Usuario usuario : usuarios) {
             if (usuario.getNombre().toLowerCase().contains(apellido.toLowerCase())) {
                 return usuario.getDui();
@@ -244,11 +249,7 @@ public class Base {
         }
         return null; // si no encuentra, retorna null
     }
-    
-    
-    
-    
-    
+
     public ArrayList<Servicio> buscarServicio(String identificador, String busca) {
         ArrayList<Servicio> temp = new ArrayList<>();
         Servicio encontrado = null;
@@ -263,16 +264,14 @@ public class Base {
         }
         return temp;
 
-    
     }
-    
-    
-     public boolean modificarServicio(String numCuenta, String nuevoIdRuta, String nuevoNumMedidor, String NuevoMetrosCubicos) {
+
+    public boolean modificarServicio(String numCuenta, String nuevoIdRuta, String nuevoNumMedidor, String NuevoMetrosCubicos) {
         for (Servicio servi : servicios) {
             if (servi.getNumeroCuenta().equals(numCuenta)) {
-                servi.setIdRuta(nuevoIdRuta); 
+                servi.setIdRuta(nuevoIdRuta);
                 servi.setNumMedidor(nuevoNumMedidor);
-                servi.setMetrosCubicos(Integer.parseInt(NuevoMetrosCubicos));                        
+                servi.setMetrosCubicos(Integer.parseInt(NuevoMetrosCubicos));
                 return true; // Se modificó con éxito
 
             }
@@ -280,7 +279,6 @@ public class Base {
         return false;
 
     }
-    
 
     public boolean modificarUsuario(String dui, String nuevoDui, String nuevoNombre, String nuevoApellido) {
         for (Usuario usuario : usuarios) {
@@ -288,7 +286,7 @@ public class Base {
                 usuario.setNombre(nuevoNombre);
                 usuario.setDui(nuevoDui);
                 usuario.setApellido(nuevoApellido);
-                        
+
                 return true; // Se modificó con éxito
 
             }
@@ -296,98 +294,89 @@ public class Base {
         return false;
 
     }
-    
-    
+
     //logica de rutas. 
-    
-    public boolean agregarRuta(ModeloRuta o){
+    public boolean agregarRuta(ModeloRuta o) {
         try {
             rutas.add(o);
-            return true; 
+            return true;
         } catch (Exception e) {
             return false;
         }
     }
-    
-    public boolean modificarRuta(String id, String depto, String muni, String col,String desc){
+
+    public boolean modificarRuta(String id, String depto, String muni, String col, String desc) {
         for (ModeloRuta ruta : rutas) {
             if (ruta.getId().equals(id)) {
                 ruta.setDepartamento(depto);
                 ruta.setColonia(col);
                 ruta.setDescripcion(desc);
                 ruta.setMunicipio(muni);
-                return true; 
+                return true;
             }
         }
-        return false; 
+        return false;
     }
-    
-    public boolean eliminarRuta (String id){
-        
+
+    public boolean eliminarRuta(String id) {
+
         for (ModeloRuta ruta : rutas) {
             if (ruta.getId().equals(id)) {
                 rutas.remove(ruta);
                 return true;
             }
-            
+
         }
         return false;
     }
 
-    public String nombre(String n){
+    public String nombre(String n) {
         for (Usuario usuario : usuarios) {
             if (usuario.getDui().equals(n)) {
                 return usuario.getNombre();
 
-            }
-            else{
+            } else {
                 return null;
             }
         }
         return null;
-        
+
     }
-    
-    public String apellido(String a){
+
+    public String apellido(String a) {
         for (Usuario usuario : usuarios) {
             if (usuario.getDui().equals(a)) {
                 return usuario.getApellido();
 
             }
-           
+
         }
         return null;
-        
+
     }
-    
-        public boolean eliminarServicio (String NumCuenta){
-        
+
+    public boolean eliminarServicio(String NumCuenta) {
+
         for (Servicio servicio : servicios) {
             if (servicio.getnumeroCuenta().equals(NumCuenta)) {
                 servicios.remove(servicio);
                 return true;
             }
-            
+
         }
         return false;
     }
-        
-        public String rutaActual (String numCuenta){
-            for (Servicio servicio : servicios) {
-                if (numCuenta.equals(servicio.getNumeroCuenta())) {
-                    return servicio.getIdRuta();
-                }
+
+    public String rutaActual(String numCuenta) {
+        for (Servicio servicio : servicios) {
+            if (numCuenta.equals(servicio.getNumeroCuenta())) {
+                return servicio.getIdRuta();
             }
-            return null; 
         }
-        
-        
-        
-        
-        
-        
-        
-         public boolean agregarConsumos(ModeloConsumo c) {
+        return null;
+    }
+
+    public boolean agregarConsumos(ModeloConsumo c) {
         try {
             consumos.add(c);
             return true;
@@ -395,14 +384,14 @@ public class Base {
             return false;
         }
     }
-         
-         public int obtenerLecturaAnterior(int numMes){
-             for (ModeloConsumo cons : consumos) {
-                 if (numMes==cons.getNumMes()) {
-                     return cons.getMetrosCubicos();
-                 }
-             }
-             return 0; 
-         }
-         
+
+    public int obtenerLecturaAnterior(int numMes) {
+        for (ModeloConsumo cons : consumos) {
+            if (numMes == cons.getNumMes()) {
+                return cons.getMetrosCubicos();
+            }
+        }
+        return 0;
+    }
+
 }
